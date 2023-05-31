425e5bbbec032285b91343c4d3d5d6a6d9a59dd3
Luis Alejandro Torres Escalera <88306161+late1x@users.noreply.github.com>
2023-05-31 00:24:19 -0600

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,25 +1,26 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST["nombre"];
-    $correo = $_POST["correo"];
-    $mensaje = $_POST["mensaje"];
+    // Configura los detalles del correo electrónico
+    $to = "alejandrotorresescalera@gmail.com"; // Reemplaza con tu dirección de correo electrónico de Gmail
+    $subject = "Mensaje desde el formulario de contacto";
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
-    $destinatario = "alejandrotorresescalera@gmail.com";
+    // Construye el cuerpo del correo electrónico
+    $body = "Nombre: $name\n\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Mensaje:\n$message";
 
-    $asunto = "Mensaje de contacto desde el sitio web";
+    // Configura las cabeceras del correo electrónico
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
 
-    $cuerpo = "Nombre: " . $nombre . "\n";
-    $cuerpo .= "Correo: " . $correo . "\n";
-    $cuerpo .= "Mensaje: " . $mensaje;
-
-    // Encabezados del correo electrónico
-    $headers = "From: " . $correo . "\r\n";
-    $headers .= "Reply-To: " . $correo . "\r\n";
-
-    if (mail($destinatario, $asunto, $cuerpo, $headers)) {
-        echo "Gracias por contactarnos. Tu mensaje ha sido enviado.";
+    // Envía el correo electrónico
+    if (mail($to, $subject, $body, $headers)) {
+        echo "El mensaje ha sido enviado correctamente.";
     } else {
-        echo "Lo sentimos, hubo un error al enviar tu mensaje. Por favor, inténtalo de nuevo.";
+        echo "Ocurrió un error al enviar el mensaje.";
     }
 }
 ?>
